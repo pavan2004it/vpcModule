9dc619eb3d3153625e290d956cabcdca2cfceecf
Pavan Kumar <pavan_2004it@icloud.com>
2024-06-11 18:00:12 +0530

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -272,3 +272,15 @@ output "nat-id" {
 output "igw-id" {
   value = aws_internet_gateway.this[*].id
 }
+
+################################################################################
+#Route Table Id's
+################################################################################
+
+output "private_route_table_id" {
+  value = aws_route_table.private_route_table.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public_route_table.id
+}
